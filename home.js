a504d804ec3202442d07f0ade79673682860a187
mdtaosim <taosimabdullah@gmail.com>
2025-08-26 22:00:11 +0600

    add the amount

--- home.js
+++ home.js
@@ -1,1 +1,13 @@
-console.log('hello I am heare')
+document.getElementById('add-mony-btn')
+    .addEventListener('click', function (e){
+        e.preventDefault();
+
+        const amount = parseInt(document.getElementById('amount-add').value);
+        const availableBalance = parseInt(document.getElementById('balance').innerText);
+
+        const totalBalance = amount + availableBalance;
+
+        document.getElementById('balance').innerText = totalBalance;
+
+        console.log(totalBalance);
+    })
