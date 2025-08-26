daf2a0d9cf127bca25c7cb4104560c7699f998bd
mdtaosim <taosimabdullah@gmail.com>
2025-08-26 22:28:24 +0600

    validation added

--- home.js
+++ home.js
@@ -1,12 +1,24 @@
 document.getElementById('add-mony-btn')
-    .addEventListener('click', function (e){
+    .addEventListener('click', function (e) {
         e.preventDefault();
-
+        const validePin = 1234;
         const amount = parseInt(document.getElementById('amount-add').value);
         const availableBalance = parseInt(document.getElementById('balance').innerText);
 
-        const totalBalance = amount + availableBalance;
+        const acountNumber = document.getElementById('bank-account-number').value;
+        const pinNumber = parseInt(document.getElementById('pin-number').value);
 
+        if (acountNumber.length < 9) {
+            alert('Please Provide a valide Account Number');
+            return;
+        }
+
+        if (pinNumber !== validePin) {
+            alert('Incorect Pin');
+            return;
+        }
+
+        const totalBalance = amount + availableBalance;
         document.getElementById('balance').innerText = totalBalance;
 
     })
